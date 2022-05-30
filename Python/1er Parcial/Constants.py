bc9b7f0fdccde3208feffbbbc6e1ee06b2e574d4
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-30 12:07:02 -0500

    Add files via upload

---
+++ Python/1er Parcial/Constants.py
@@ -0,0 +1,9 @@
+print('aprovados')
+print(5)
+mensaje='apravad'
+mensaje2='reprovados'
+print(mensaje)
+print(mensaje2)
+mensaje='menos tarea para el 9 virtual'
+print(mensaje)
+print('este es el mensaje guardado en memoria:',mensaje)
