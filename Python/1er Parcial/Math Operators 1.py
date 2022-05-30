43cbff28ffdb974baee7994e65011739f98fbfbe
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-30 12:13:18 -0500

    Add files via upload

---
+++ Python/1er Parcial/Math Operators 1.py
@@ -0,0 +1,27 @@
+Math=8
+Math1=5
+Math2=7
+Promedio=(Math+Math1+Math2)/3
+print('el promedio de matematicas es',Promedio)
+Esp=6
+Esp1=9
+Esp2=7
+Promedio2=(Esp+Esp1+Esp2)/3
+print("el promedio de español es",Promedio2)
+Qui=4
+Qui1=7
+Qui2=8
+Promedio3=(Qui+Qui1+Qui2)/3
+print('el promedio de quimica es',Promedio3)
+In=5
+In1=4
+In2=7
+Promedio4=(In+In1+In2)/3
+print('el promedio de informatica es',Promedio4)
+MCT=8
+MCT1=6
+MCT2=7
+Promedio5=(MCT+MCT1+MCT2)/3
+print('el promedio de mecatronica es',Promedio5)
+Prom=(Promedio+Promedio2+Promedio3+Promedio4+Promedio5)/5
+print('el promedio total es',Prom)
