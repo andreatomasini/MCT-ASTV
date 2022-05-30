df6c1ece3f46628491a1b4fda62535b20a9312a9
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-30 12:09:50 -0500

    Add files via upload

---
+++ Python/1er Parcial/Using constants.py
@@ -0,0 +1,20 @@
+Name='Sofía'
+print('my name is:',Name)
+Grade='9'
+print('my grade is:',Grade)
+Group='D'
+print('my group is:',Group)
+Comida='Sopa de tortilla'
+print('my favorite food is:',Comida)
+Comentario='estoy en mi casa'
+print(Comentario)
+name='Dayana'
+print('my name is:',name)
+grade='9'
+print('my grade is:',grade)
+group='D'
+print('my group is:',group)
+Food='spaghetti'
+print('my favorite food is:',Food)
+comentario='estoy en mi casa'
+print(comentario)
