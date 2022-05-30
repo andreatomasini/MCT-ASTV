cfded88a5a2e965aaf58f0c0d47abfcc689fdbef
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-30 12:19:18 -0500

    Add files via upload

---
+++ Python/1er Parcial/Input-Review.py
@@ -0,0 +1,13 @@
+saludo='hola'
+print(saludo)
+
+print('ingrese su saludo')
+saludo=input()
+print("escribiste",saludo)
+print("escribe tu nombre")
+
+nombre=input()
+print("tu mombre es",nombre)
+apellido=input("escribe tu apellido:")
+print("tu apellido es ", apellido)
+
