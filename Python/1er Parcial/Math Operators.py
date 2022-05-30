7eef6a56d45d899f93b510fbabcd32923daf2fbf
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-30 12:11:40 -0500

    Add files via upload

---
+++ Python/1er Parcial/Math Operators.py
@@ -0,0 +1,29 @@
+trombones76='hola'
+more=1000000
+clase='hello there'
+
+#math operators
+
+alpha=2
+b3t4=6
+suma=alpha+b3t4
+print('el resultado de la suma fue',suma)
+resta=alpha-b3t4
+print('el resultado de la resta fue',resta)
+multiplicacion=alpha*b3t4
+print('resultado de multiplicacion fue',multiplicacion)
+division=alpha/b3t4
+print('el resultado de la division fue',division)
+potencia=2**3
+print('resultado',potencia)
+
+
+
+
+#code for the week, promedio de cinco materias y luego promedio general con la suma de los tres trimestres en cada materia.
+mct=8
+mct1=9
+mct2=5
+suma1=mct+mct1+mct2
+promedio=suma1/3
+print('el promedio es',promedio)
