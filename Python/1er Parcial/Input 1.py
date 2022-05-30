79ca63f70ac941dd404c094341c529b901aeea62
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-30 12:24:14 -0500

    Add files via upload

---
+++ Python/1er Parcial/Input 1.py
@@ -0,0 +1,42 @@
+print("Hello")
+"1"
+print("What is your favorite color?")
+color=input()
+print("My favorite color is:",color)
+"2"
+print("Where are you from?")
+nationality=input()
+print("I am from:",nationality)
+"3"
+print("What is your favorite animal?")
+animal=input()
+print("My favorite animal is:",animal)
+"4"
+print("What is your name?")
+why=input()
+print("It is:",why)
+"5"
+print("How do you describe your personality?")
+personality=input()
+print("I describe it as:",personality)
+"6"
+print("What are your favorite hobbies?")
+hobbies=input()
+print("My hobbies are:",hobbies)
+"7"
+print("How do you describe yourself physically?")
+physically=input()
+print("I describe myself as:",physically)
+"8"
+print("Are you man or woman?")
+gender=input()
+print("I am a:",gender)
+"9"
+print("Do you study, work or none of those options?")
+ocupation=input()
+print("I:",ocupation)
+"10"
+print("What is your favorite food?")
+food=input()
+print("My favorite food is:",food)
+print("END")
