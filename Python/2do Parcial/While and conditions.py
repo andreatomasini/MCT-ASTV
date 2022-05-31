dcd664bf4ed9def6692bba6beb9fe9591a4f5c14
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-31 10:02:18 -0500

    Add files via upload

---
+++ Python/2do Parcial/While and conditions.py
@@ -0,0 +1,11 @@
+#code 2
+comparator=0
+while(comparator<=14):
+  print("press the button")
+  print("selled")
+  comparator=comparator+1
+
+print("Oh no,we don't have more tiket...")
+print("So sorry, come back later...")
+  
+
