b12e075b0248aaaad425b2142beb57d7a7df7d85
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-31 09:41:31 -0500

    Add files via upload

---
+++ Python/2do Parcial/Int 1.py
@@ -0,0 +1,15 @@
+#CLASSCODE: 181121
+
+nombre=input("ingrese su nombre")
+print("saludos",nombre)
+
+pais=input("de que pais sos vos?")
+print(pais,"es interesante")
+
+print(nombre,"es de ",pais,"y es buena onda")
+
+#int sirve oara decirle a input( que debe leer )
+numero=int(input("ingrese numero"))
+numero2=int(input("ingrese numero"))
+
+print("la suma de estos numeros es:",(numero+numero2))
