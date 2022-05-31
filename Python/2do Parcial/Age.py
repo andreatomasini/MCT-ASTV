1abb1c0426edd7ff30e58d967c97ca08e4763fb0
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-31 09:48:55 -0500

    Add files via upload

---
+++ Python/2do Parcial/Age.py
@@ -0,0 +1,15 @@
+#Sacar meses,semanas,días,horas y segundos
+age=int(input("Write your age:"))
+mes=age*12
+print("Your age in months on avarage is:",mes)
+semanas=mes*4
+print("Your age in weeks on avarage is:",semanas)
+días=365*age
+print("Your age in days on avarage is:",días)
+horas=días*24
+print("Your age in hours on avarage is:",horas)
+minutos=horas*60
+print("Your age in minuts on avarage is:",minutos)
+Segundos=minutos*60
+print("Your age in seconds on avarage is:",Segundos)
+#End
