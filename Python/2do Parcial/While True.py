c3a6b5b1af62014a5bc8461951463fad8f47894f
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-31 09:54:57 -0500

    Add files via upload

---
+++ Python/2do Parcial/While True.py
@@ -0,0 +1,7 @@
+while True:
+#identacio
+    print("hola que hace") 
+    print("adios")
+    mensaje=input("di algo")
+    print("reprobados")
+#Volver infinito el codigo de no,bre grado y grupo,etc...
