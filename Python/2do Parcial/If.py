509ecf817ab7ae4e65c176f1681aff7205493e1e
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-31 10:52:15 -0500

    Add files via upload

---
+++ Python/2do Parcial/If.py
@@ -0,0 +1,15 @@
+x=int(input("Write a number:"))
+if x<10:
+  print('smaller')
+if x>20:
+  print('bigger')
+
+print('finis')
+
+x=5
+if x<10:
+  print('smaller')
+if x>20:
+  print('bigger')
+
+print('finis')
