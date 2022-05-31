83c136602b7d8b9f991e343898e5de0cdc389618
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-05-31 09:51:58 -0500

    Add files via upload

---
+++ Python/2do Parcial/While .py
@@ -0,0 +1,6 @@
+while True:
+  name=input("Write your name:")
+  grade=input("Write your grade:")
+  group=input("Write you group:")
+  food=input("Write your favorite food:")
+  things=input("Write something interesing about you:")
