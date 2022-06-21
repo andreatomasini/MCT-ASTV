bc5ec4205b06d4e027e98028956789fc6459e89f
andreatomasini <104382471+andreatomasini@users.noreply.github.com>
2022-06-21 09:59:13 -0500

    Add files via upload

---
+++ Python/3er Parcial/And Or 1.py
@@ -0,0 +1,8 @@
+while True:
+   x=int(input("Write the first number:"))
+   y=int(input("Write the second number:"))
+   if(x==6 and y==10):
+    print("Se cumplen ambas condiciones")
+
+   if(x>0 or y<10):
+    print("Se cumple una de las condiciones")
